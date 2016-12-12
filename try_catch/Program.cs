31964b6346eecef53a8de4c4e4b2d10e6d3bcada
Patrick Todd <patrick.todd@reisystems.com>
2016-12-12 14:19:46 -0500

    playing with try/catch exception handling

---
+++ try_catch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try this code
+            //if successful, jump to the end of the try/catch block
+            //but if there are exceptions 
+            //execute code in catch block
+            try
+            {
+                string[] fruit = { "Apples", "Bananas", "Oranges" };
+                for (int i = 0; i <= fruit.Length; i++)
+                {
+                    Console.WriteLine(fruit[i]);
+                }
+            }
+            //name the exception and console it
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
